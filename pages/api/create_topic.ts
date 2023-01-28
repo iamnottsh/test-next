e9e218f2f84db10650ac273539d8a5826f69b1dc
gxz <cnmmail163@163.com>
2023-01-29 03:37:02 +0800

    完善接口

--- pages/api/create_topic.ts
+++ pages/api/create_topic.ts
@@ -2,9 +2,13 @@ import {NextApiRequest, NextApiResponse} from "next";
 import _db from "@/_db";
 import _limit from "@/_limit";
 
+export async function topics() {
+    return (await _db()).db('content').collection<{ id: string, key: string, value: string }>('topics')
+}
+
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<{}>
+    res: NextApiResponse
 ) {
     try {
         const {id, key, value} = req.body
@@ -18,14 +22,9 @@ export default async function handler(
             res.status(406).send('你在逗我？')
             return
         }
-        await (await _db())
-            .db('content')
-            .collection<{ id: string, key: string, value: string }>('topics')
-            .updateOne(
-                {id},
-                {$setOnInsert: {id, key, value}},
-                {upsert: true}
-            )
+        const collection = await topics()
+        await collection
+            .updateOne({id}, {$setOnInsert: {id, key, value}}, {upsert: true})
         res.status(200).json({})
     } catch (e) {
         res.status(403).send(String(e))
--- pages/api/get_topics.ts
+++ pages/api/get_topics.ts
@@ -1,11 +1,11 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import _db from "@/_db";
 import {ObjectId} from "mongodb";
 import _stack from "@/_stack";
+import {topics} from "@/pages/api/create_topic";
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<{}>
+    res: NextApiResponse
 ) {
     try {
         const {no, sort} = req.body
@@ -14,14 +14,16 @@ export default async function handler(
             res.status(406).send('你在逗我？')
             return
         }
-        await (await _db())
-            .db('content')
-            .collection<{ id: string, key: string, value: string }>('topics')
-            .find(_id ? {_id: {"$gt": _id}} : {})
-            .sort({_id: sort})
-            .limit(_stack)
-            .toArray()
-        res.status(200).json({})
+        const collection = await topics()
+        res.status(200).json({
+            array: await collection
+                .find(_id ? {_id: {"$gt": _id}} : {})
+                .sort({_id: sort})
+                .limit(_stack)
+                .toArray(),
+            total: await collection
+                .countDocuments()
+        })
     } catch (e) {
         res.status(403).send(String(e))
     }
