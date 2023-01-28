7a308abf64ff8d99c5cf3a44b55149481e064d02
gxz <cnmmail163@163.com>
2023-01-28 22:02:04 +0800

    处理错误

--- pages/api/create_topic.ts
+++ pages/api/create_topic.ts
@@ -5,21 +5,25 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<{}>
 ) {
-    const {id, key, value} = req.body
-    if (typeof id !== 'string'
-        || id.length > 8192
-        || typeof key !== 'string'
-        || key.length > 8192
-        || typeof value !== 'string'
-        || value.length > 8192
-    ) return
-    await (await _db())
-        .db('content')
-        .collection<{ id: string, key: string, value: string }>('topics')
-        .updateOne(
-            {id},
-            {$setOnInsert: {id, key, value}},
-            {upsert: true}
-        )
-    res.status(200).json({})
+    try {
+        const {id, key, value} = req.body
+        if (typeof id !== 'string'
+            || id.length > 8192
+            || typeof key !== 'string'
+            || key.length > 8192
+            || typeof value !== 'string'
+            || value.length > 8192
+        ) return
+        await (await _db())
+            .db('content')
+            .collection<{ id: string, key: string, value: string }>('topics')
+            .updateOne(
+                {id},
+                {$setOnInsert: {id, key, value}},
+                {upsert: true}
+            )
+        res.status(200).json({})
+    } catch (e) {
+        res.status(403).json({e})
+    }
 }
